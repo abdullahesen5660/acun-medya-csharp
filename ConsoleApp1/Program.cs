3592bcd04e7212707929a2e822c9df428eb2211e
Abdullah esen <abdullah.esen5660@gmail.com>
2025-03-18 19:30:02 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Marka togg = new Marka("togg");
+        string model = "t10x";
+        Kapi kapi = new Kapi(4);
+        Pencere pencere = new Pencere(4);
+        Kasa kasa = new Kasa("suv");
+        decimal fiyat = 1649000m;
+
+        Araba bmwX5 = new Araba(togg, model, kapi, pencere, kasa, fiyat);
+
+        bmwX5.ArabaBilgisiYazdir();
+    }
+}
